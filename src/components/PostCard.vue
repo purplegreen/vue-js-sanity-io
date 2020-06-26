9ec7de5425168740f7f2946e3193412288f76d83
dehlix <deborahligorio@gmail.com>
2020-06-26 15:22:08 +0200

    log-post

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -91,6 +91,7 @@ export default {
       sanity.fetch(query).then(
         post => {
           this.loading = false;
+          console.log("post", post);
           this.post = post;
           // this.texts = this.post.title;
         },
